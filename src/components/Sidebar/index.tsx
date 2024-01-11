3966dd2f845a9d3fe8a76ed584a8c71aac5c7595
Alexsander Telles <alevtelles@gmail.com>
2024-01-11 15:08:01 -0300

    update sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,12 +1,23 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
 import { Logo } from "./Logo";
-import { Search } from "lucide-react";
-import { MainNavigation } from "./MainNavigation";
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users,
+} from "lucide-react";
+import { NavItem } from "./NavItem";
+import { UsedSpaceWidget } from "./UsedSpaceWidget";
 
 export function Sidebar() {
   return (
-    <div className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <div className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
       <div className=" flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
         <Search className="mx-1 h-5 w-5 text-zinc-500" />
@@ -15,7 +26,22 @@ export function Sidebar() {
           placeholder="Pesquisar"
         />
       </div>
-      <MainNavigation />
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+
+        <UsedSpaceWidget />
+      </div>
     </div>
   );
 }
